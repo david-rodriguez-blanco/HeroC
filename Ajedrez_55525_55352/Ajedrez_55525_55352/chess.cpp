48ee5905a079beb728a4446e9dc0e47df2e1497c
nikitazhukov2911 <61113968+nikitazhukov2911@users.noreply.github.com>
2022-04-24 13:32:39 +0200

    Update chess.cpp

--- Ajedrez_55525_55352/Ajedrez_55525_55352/chess.cpp
+++ Ajedrez_55525_55352/Ajedrez_55525_55352/chess.cpp
@@ -3,25 +3,98 @@
 #include <cmath>
 #include <string>
 
+//Constructor Casilla
+
+void Casilla::setVacio()
+{
+	color = NS;
+	figura = VACIO;
+}
+void Casilla::setCas(Casilla* cas)
+{
+	color = cas->getColor();
+	figura = cas->getFigura();
+}
+
+
+
+Figura Casilla::getFigura()
+{
+	return figura;
+}
+
+Color Casilla::getColor()
+{
+	return color;
+}
+
+//Funcion setter para X
+void Casilla :: sX(int _x)
+{
+	x = _x;
+}
+//Funcion setter para y
+void Casilla::sY(int _y)
+{
+	y = _y;
+}
+//Funcion getter para X
+int Casilla::gX()
+{
+	return x;
+}
+//Funcion getter para X
+int  Casilla::gY()
+{
+	return y;
+}
+
+//Destructor Casilla
+
+Casilla::~Casilla()
+{
+
+}
 
 
 void Tabla::setTabla()
 {
+	//Las figuras blacncas y negras en las filas 0 y 7
 	casilla[0][0].setFAC(TORRE, BLANCO);
-	casilla[0][1].setFAC(CABALLO, BLANCO);
-	casilla[0][2].setFAC(ALFIL, BLANCO);
-	casilla[0][3].setFAC(REINA, BLANCO);
-	casilla[0][4].setFAC(REY, BLANCO);
-	casilla[0][5].setFAC(, BLANCO);
-	casilla[0][6].setFAC(TORRE, BLANCO);
+	casilla[1][0].setFAC(CABALLO, BLANCO);
+	casilla[2][0].setFAC(ALFIL, BLANCO);
+	casilla[3][0].setFAC(REINA, BLANCO);
+	casilla[4][0].setFAC(REY, BLANCO);
+	casilla[5][0].setFAC(ALFIL, BLANCO);
+	casilla[6][0].setFAC(CABALLO, BLANCO);
+	casilla[7][0].setFAC(TORRE, BLANCO);
+
 	casilla[0][7].setFAC(TORRE, BLANCO);
+	casilla[1][7].setFAC(CABALLO, BLANCO);
+	casilla[2][7].setFAC(ALFIL, BLANCO);
+	casilla[3][7].setFAC(REINA, BLANCO);
+	casilla[4][7].setFAC(REY, BLANCO);
+	casilla[5][7].setFAC(ALFIL, BLANCO);
+	casilla[6][7].setFAC(CABALLO, BLANCO);
+	casilla[7][7].setFAC(TORRE, BLANCO);
 
-	casilla[0][0].setFAC(TORRE, BLANCO);
-	casilla[0][0].setFAC(TORRE, BLANCO);
-	casilla[0][0].setFAC(TORRE, BLANCO);
-	casilla[0][0].setFAC(TORRE, BLANCO);
-	casilla[0][0].setFAC(TORRE, BLANCO);
+	//El resto de la tabla en blanco sin figuras
+	for (int j = 0; j <= 5; j++)
+	{
+		for (int k = 0; k <= 7; k++)
+		{
+			casilla[k][j].setFAC(VACIO,NS);
+		}
+	}
 
+	//Peones blancos y negros
+	for (int i = 0; i <= 7; i++)
+	{
+		casilla[i][1].setFAC(PEON, BLANCO);
+		casilla[i][6].setFAC(PEON, NEGRO);
+	}
+
+}
 
 	//prototipo función menú
 
@@ -42,4 +115,4 @@ int menu()
 		ranking();//aun sin definir
 }
 	
-}
+

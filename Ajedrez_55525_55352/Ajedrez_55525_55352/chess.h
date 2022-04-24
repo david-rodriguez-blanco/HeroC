52449054f2cae7502bfbc74fe39179365105007d
nikitazhukov2911 <61113968+nikitazhukov2911@users.noreply.github.com>
2022-04-24 13:33:52 +0200

    Update chess.h

--- Ajedrez_55525_55352/Ajedrez_55525_55352/chess.h
+++ Ajedrez_55525_55352/Ajedrez_55525_55352/chess.h
@@ -1,8 +1,12 @@
 #pragma once
+//Cabecera
+#include <iostream>
+#include <string.h>
+#include <math.h>
 
 
 	enum Figura { REY, REINA, ALFIL, CABALLO, TORRE, PEON, VACIO };
-	enum Color { BLANCO, NEGRO, VACIO };
+	enum Color { BLANCO, NEGRO, NS};
 
 	class Casilla
 	{
@@ -11,43 +15,62 @@
 		Color color;
 		int x, y;
 	public:
+		Casilla();		//Constructor Casilla
+		void setCas(Casilla*);
+		void setVacio();
 		void setFAC(Figura, Color);
-
+		Figura getFigura();
+		Color getColor();
+		void sX(int _x);
+		void sY(int _y);
+		int gX();
+		int gY();
+		~Casilla(); //Destructor Casilla
 	};
 	class Tabla
 	{
 		Casilla casilla[8][8];
-		//Color def = BLANCO;
-
-
-
+		Color def = BLANCO;
 		//funciones para mover figuras
-		bool moverRey(Casilla* thisRey, Casilla* thisSpace);
+		// 
+		// 
+		// 
+		//bool moverRey(Casilla* thisRey, Casilla* thisSpace);
+		//
+		//
+		//
+		//
+		//
+
+
+
 
 
 		/*Aniadir mov para el resto de figuras*/
 
+
+
+
 		//obtener la posicion de la casilla
-		//devuelve dato de tipo casilla 
 		//Getter
-			Casilla* getCasilla(int x, int y)
+		Casilla* getCasilla(int x, int y)
 		{
 			return &casilla[x][y];
-		}
+		};
 		//establecer la posicion de la casilla
 		//Setter
-			void setCasilla(Casilla* c, int x, int y)
-			{
-				casilla[x][y] = *c;
-			}
+		void setCasilla(Casilla* c, int x, int y)
+		{
+			casilla[x][y] = *c;
+		}
 		//Funcion para realizar movimiento
 			bool hacerMov();
 		//Funcion para establecer la tabla 
-			void setTabla();
-		// bool jugarAjedrez(); ????
+	         void setTabla();
+		 bool jugarAjedrez(); 
+
 
 
 
 	};
-
 
